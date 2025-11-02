7e010d2f93615b7f65b3c5feccf1f55099e0939e
Rafa Cruz <jose.cruz23@ulatina.net>
2025-11-02 14:33:27 -0600

    Instrucciones

---
+++ Tarea 1/Tarea 1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Tarea_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Codigo que pregunte por numeros aleatorios y los ponga en una lista
+            // luego se le solicita al usuario un el numero de la posiciones de la lista
+            // Sumaria los nuemros de la posicion correspondiente
+
+            // Tomar en cuenta errores como lestras, numeros fuera de rango, espacios vacios, etc.
+
+            /*
+            * O tambien podemos preguntar por nombres, luego pedir las edades de cada uno y luego imprimrlas una por una.
+            */
+
+
+
+            /*
+             * Recordar el TryParse, Listas, manejo de errores con try catch, ciclos, condicionales, etc.
+             */
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
