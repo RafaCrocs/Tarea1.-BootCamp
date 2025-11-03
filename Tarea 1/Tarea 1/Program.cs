bd27e6ba5a600a8fcdb99de3e9bdecf2135dabad
Rafa Cruz <jose.cruz23@ulatina.net>
2025-11-02 23:51:22 -0600

    Avance

--- Tarea 1/Tarea 1/Program.cs
+++ Tarea 1/Tarea 1/Program.cs
@@ -19,7 +19,66 @@
             /*
              * Recordar el TryParse, Listas, manejo de errores con try catch, ciclos, condicionales, etc.
              */
-            Console.WriteLine("Hello, World!");
+            bool a = true;
+            try
+            {
+                do
+                {
+                    List<int> numeros = new List<int>();
+                    Console.WriteLine("Ingrese una lista de numeros...\n");
+
+                    Console.Write("Ingrese el primer numero: ");
+                    int num1 = int.Parse(Console.ReadLine());
+                    numeros.Add(num1);
+
+                    Console.Write("Ingrese el segundo numero: ");
+                    int num2 = int.Parse(Console.ReadLine());
+                    numeros.Add(num2);
+
+                    Console.Write("Ingrese el tercer numero: ");
+                    int num3 = int.Parse(Console.ReadLine());
+                    numeros.Add(num3);
+
+                    
+                    for (int i = 0;i < numeros.Count(); i++)
+                    {
+                        Console.WriteLine($"{i + 1}. {numeros[i]}");
+                    }
+                    
+                    while (a)
+                    {
+                        Console.WriteLine("Ingrese la posicion del numero que desea cambiar (1, 2 o 3): \nENTER para omitir...");
+                        int.TryParse(Console.ReadLine(), out int elmposicion);
+                        if (elmposicion < 0 || elmposicion > 3)
+                        {
+                            Console.WriteLine("Posicion fuera de rango. Intente de nuevo.");
+                        }
+                        else if (elmposicion == 0)
+                        {
+                            Console.WriteLine("Saliendo del programa...");
+                            a = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Numero eliminado... Ingrese el nuevo numero: ");
+                            int nuevonumero = int.Parse(Console.ReadLine());
+                            numeros[elmposicion - 1] = nuevonumero;
+
+                            for (int i = 0; i < numeros.Count(); i++)
+                            {
+                                Console.WriteLine($"{i + 1}. {numeros[i]}");
+                            }
+                        }
+                    }
+
+                }
+                while (a);
+            }
+            catch
+            {
+                Console.WriteLine("Ha ocurrido un error.");
+            }
+            Console.WriteLine("Fin///");
         }
     }
 }
