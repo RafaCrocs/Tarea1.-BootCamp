56719e3406d8ae0e351adf8c8c1f825d3c832f77
Rafa Cruz <jose.cruz23@ulatina.net>
2025-11-03 21:44:01 -0600

    Avance 2

--- Tarea 1/Tarea 1/Program.cs
+++ Tarea 1/Tarea 1/Program.cs
@@ -2,6 +2,26 @@
 {
     internal class Program
     {
+
+
+        public static int Revision()
+        {
+            while (true)
+            {
+                int.TryParse(Console.ReadLine(), out int y);
+                if (y == 0)
+                {
+                    Console.WriteLine("Ingrese un numero valido...");
+                }
+                else
+                {
+                    return y;
+                    break;
+                }
+            }
+        }
+
+
         static void Main(string[] args)
         {
             // Codigo que pregunte por numeros aleatorios y los ponga en una lista
@@ -11,72 +31,78 @@
             // Tomar en cuenta errores como lestras, numeros fuera de rango, espacios vacios, etc.
 
             /*
-            * O tambien podemos preguntar por nombres, luego pedir las edades de cada uno y luego imprimrlas una por una.
+            O tambien podemos preguntar por nombres, luego pedir las edades de cada uno y luego imprimrlas una por una.
             */
 
 
 
             /*
-             * Recordar el TryParse, Listas, manejo de errores con try catch, ciclos, condicionales, etc.
-             */
+            Recordar el TryParse, Listas, manejo de errores con try catch, ciclos, condicionales, etc.
+            */
             bool a = true;
             try
             {
-                do
+                List<int> numeros = new List<int>();
+                Console.WriteLine("Ingrese una lista de numeros...\n");
+
+                // Ingreso de numeros
+                Console.Write("Ingrese el primer numero: ");
+                int num1 = Revision();
+                numeros.Add(num1);
+
+                Console.Write("Ingrese el segundo numero: ");
+                int num2 = Revision();
+                numeros.Add(num2);
+
+                Console.Write("Ingrese el tercer numero: ");
+                int num3 = Revision();
+                numeros.Add(num3);
+
+                // Mostrar lista inicial
+                for (int i = 0; i < numeros.Count(); i++)
                 {
-                    List<int> numeros = new List<int>();
-                    Console.WriteLine("Ingrese una lista de numeros...\n");
-
-                    Console.Write("Ingrese el primer numero: ");
-                    int num1 = int.Parse(Console.ReadLine());
-                    numeros.Add(num1);
-
-                    Console.Write("Ingrese el segundo numero: ");
-                    int num2 = int.Parse(Console.ReadLine());
-                    numeros.Add(num2);
-
-                    Console.Write("Ingrese el tercer numero: ");
-                    int num3 = int.Parse(Console.ReadLine());
-                    numeros.Add(num3);
-
-                    
-                    for (int i = 0;i < numeros.Count(); i++)
-                    {
-                        Console.WriteLine($"{i + 1}. {numeros[i]}");
-                    }
-                    
-                    while (a)
-                    {
-                        Console.WriteLine("Ingrese la posicion del numero que desea cambiar (1, 2 o 3): \nENTER para omitir...");
-                        int.TryParse(Console.ReadLine(), out int elmposicion);
-                        if (elmposicion < 0 || elmposicion > 3)
-                        {
-                            Console.WriteLine("Posicion fuera de rango. Intente de nuevo.");
-                        }
-                        else if (elmposicion == 0)
-                        {
-                            Console.WriteLine("Saliendo del programa...");
-                            a = false;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Numero eliminado... Ingrese el nuevo numero: ");
-                            int nuevonumero = int.Parse(Console.ReadLine());
-                            numeros[elmposicion - 1] = nuevonumero;
-
-                            for (int i = 0; i < numeros.Count(); i++)
-                            {
-                                Console.WriteLine($"{i + 1}. {numeros[i]}");
-                            }
-                        }
-                    }
-
+                    Console.WriteLine($"{i + 1}. {numeros[i]}");
                 }
-                while (a);
+
+                // Modificacion de numeros
+                while (a)
+                {
+                    Console.WriteLine("Ingrese la posicion del numero que desea cambiar (1, 2 o 3): \nENTER para omitir...");
+                    int.TryParse(Console.ReadLine(), out int elmposicion);
+                    if (elmposicion < 0 || elmposicion > 3)
+                    {
+                        Console.WriteLine("Posicion fuera de rango. Intente de nuevo.");
+                    }
+                    else if (elmposicion == 0)
+                    {
+                        Console.WriteLine("Saliendo del programa...");
+                        a = false;
+                    }
+                    else
+                    {
+                        // Cambiar numero en la posicion indicada
+                        Console.WriteLine("Numero eliminado... Ingrese el nuevo numero: ");
+                        int nuevonumero = int.Parse(Console.ReadLine());
+                        numeros[elmposicion - 1] = nuevonumero;
+
+                        for (int i = 0; i < numeros.Count(); i++)
+                        {
+                            Console.WriteLine($"{i + 1}. {numeros[i]}");
+                        }
+                    }
+                }
+                // Lista Final
+                Console.WriteLine("Lista final de numeros:");
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {numeros[i]}");
+                }
+
             }
             catch
             {
                 Console.WriteLine("Ha ocurrido un error.");
+                Console.WriteLine("Intente nuevamente...");
             }
             Console.WriteLine("Fin///");
         }
