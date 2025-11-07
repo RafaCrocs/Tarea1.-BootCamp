ad2668cb285a9c74dbbe62a52f70fb07d1eb9556
Rafa Cruz <jose.cruz23@ulatina.net>
2025-11-06 23:04:24 -0600

    Creacion de NombreEdad

---
+++ NombreEdad/NombreEdad/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NombreEdad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Proyecto para Nombres y Edades");
+        }
+    }
+}
